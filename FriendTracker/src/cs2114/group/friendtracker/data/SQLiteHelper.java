c61e99a32ecb8408efe4b28fb2b1d36ddf494e6c
Tianyu Geng <g.tyu90@gmail.com>
2012-04-16 22:12:21 -0400

    testing github

--- FriendTracker/src/cs2114/group/friendtracker/data/SQLiteHelper.java
+++ FriendTracker/src/cs2114/group/friendtracker/data/SQLiteHelper.java
@@ -13,36 +13,43 @@ import android.util.Log;
  */
 public class SQLiteHelper extends SQLiteOpenHelper {
 
-	public static final String TABLE_EVENTS = "events";
-	public static final String E_ID = "_id";
-	public static final String E_NAME = "name";
-	//public static final String
+    public static final String TABLE_EVENTS = "events";
+    public static final String E_ID = "_id";
+    public static final String E_NAME = "name";
+    // public static final String
 
-	private static final String DATABASE_NAME = "friendtracker.db";
-	private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "friendtracker.db";
+    private static final int DATABASE_VERSION = 1;
 
-	// Database creation sql statement
-	private static final String DATABASE_CREATE = "create table "
-			+ TABLE_EVENTS + "( " + E_ID
-			+ " integer primary key autoincrement, " + E_NAME
-			+ " text not null);";
+    // Database creation sql statement
+    private static final String DATABASE_CREATE = "create table "
+            + TABLE_EVENTS + "( " + E_ID
+            + " integer primary key autoincrement, " + E_NAME
+            + " text not null);";
 
-	public SQLiteHelper(Context context) {
-		super(context, DATABASE_NAME, null, DATABASE_VERSION);
-	}
+    /**
+     * The constructor for this helper.
+     *
+     * @param context
+     */
+    public SQLiteHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
-	@Override
-	public void onCreate(SQLiteDatabase database) {
-		database.execSQL(DATABASE_CREATE);
-	}
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(DATABASE_CREATE);
+    }
 
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		Log.w(SQLiteHelper.class.getName(),
-				"Upgrading database from version " + oldVersion + " to "
-						+ newVersion + ", which will destroy all old data");
-		db.execSQL("DROP TABLE IF EXISTS " + TABLE_EVENTS);
-		onCreate(db);
-	}
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion,
+            int newVersion) {
+        Log.w(SQLiteHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion
+                        + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_EVENTS);
+        onCreate(db);
+    }
 
 }
