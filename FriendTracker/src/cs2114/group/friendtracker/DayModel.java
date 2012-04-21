0729fe4a8e7b5ea0d870704a8904e494d302413b
Tianyu Geng <g.tyu90@gmail.com>
2012-04-21 00:24:44 -0400

    craete a dayview.xml layout file and a DayModel class

---
+++ FriendTracker/src/cs2114/group/friendtracker/DayModel.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package cs2114.group.friendtracker;
+
+import java.util.List;
+
+/**
+ *
+ * @author Tianyu Geng (tony1)
+ * @version Apr 20, 2012
+ */
+public class DayModel {
+    private List<Event> events;
+
+    public DayModel(List<Event> events) {
+        this.events = events;
+    }
+
+}
